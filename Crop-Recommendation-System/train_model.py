6f6ff197da6196a69a79a1f5c4fd2ae86da7700b
sarathkunar <sarathkumarm478@gmail.com>
2026-05-19 16:53:44 +0530

    Add files via upload

--- Crop-Recommendation-System/train_model.py
+++ Crop-Recommendation-System/train_model.py
@@ -4,20 +4,24 @@ import pickle
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import StandardScaler
-
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 
-# Load Dataset
+# Load dataset
 df = pd.read_csv("crop_recommendation.csv")
 
 # Remove duplicates
 df.drop_duplicates(inplace=True)
 
-# Encode crop labels
+# Encode season column
+season_encoder = LabelEncoder()
+
+df['season'] = season_encoder.fit_transform(df['season'])
+
+# Encode crop label
 crop_encoder = LabelEncoder()
 
-df["label"] = crop_encoder.fit_transform(df["label"])
+df['label'] = crop_encoder.fit_transform(df['label'])
 
 # Features
 X = df.drop("label", axis=1)
@@ -30,7 +34,7 @@ scaler = StandardScaler()
 
 X_scaled = scaler.fit_transform(X)
 
-# Split dataset
+# Train Test Split
 X_train, X_test, y_train, y_test = train_test_split(
     X_scaled,
     y,
@@ -40,7 +44,7 @@ X_train, X_test, y_train, y_test = train_test_split(
 
 # Model
 model = RandomForestClassifier(
-    n_estimators=300,
+    n_estimators=200,
     random_state=42
 )
 
@@ -48,16 +52,22 @@ model = RandomForestClassifier(
 model.fit(X_train, y_train)
 
 # Predict
-predictions = model.predict(X_test)
+y_pred = model.predict(X_test)
 
 # Accuracy
-accuracy = accuracy_score(y_test, predictions)
+accuracy = accuracy_score(y_test, y_pred)
 
 print("Accuracy:", accuracy)
 
-# Save files
+# Save model
 pickle.dump(model, open("crop_model.pkl", "wb"))
+
+# Save scaler
 pickle.dump(scaler, open("scaler.pkl", "wb"))
+
+# Save encoders
+pickle.dump(season_encoder, open("season_encoder.pkl", "wb"))
+
 pickle.dump(crop_encoder, open("crop_encoder.pkl", "wb"))
 
 print("Model Saved Successfully")
